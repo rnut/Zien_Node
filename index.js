26123eb32df7d25b3990d937fcc0ecf07f5fc042
rnut <rnut.ist@gmail.com>
2015-03-24 17:15:38 +0700

    connect to mongodb
    เชื่อมต่อ db ได้แล้ว และสามารถ manipulation ได้

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ var users = require('./users');
 /* โหลด Express มาใช้งาน */
 var app = require('express')();
  var bodyParser = require('body-parser');
+
+ var mongojs = require('db');
+ var db = mongojs.connect;
 /* ใช้ port 7777 หรือจะส่งเข้ามาตอนรัน app ก็ได้ */
 var port = process.env.PORT || 7777;
 // parse application/json
@@ -26,24 +29,34 @@ app.get('/index', function (req, res) {
 
 
 app.get('/user', function (req, res) {
-    res.json(users.findAll());
+    db.users.find(function(err,docs){
+    	res.json(docs);
+    });
 });
  
 app.get('/user/:id', function (req, res) {
     var id = req.params.id;
     res.json(users.findById(id));
+    
 });
 
 
 
-app.post('/newuser', function (req, res,next) {
-	// res.contentType('application/json');
-	var json = req.body;
-    res.send(json);
- //    // res.send('Add new ' + json.name + ' Completed!');
-    console.log("body : "+json.position);
+app.get('/newuser', function (req, res,next) {
+	// // res.contentType('application/json');
+	// var json = req.body;
  //    res.send(json);
-  // next();
+ // //    // res.send('Add new ' + json.name + ' Completed!');
+ //    console.log("body : "+json.position);
+ // //    res.send(json);
+ //  // next();
+ 	db.users.insert([{name:"newUser",age:55},{name:"newUser2",age:60}],function(err,docs){
+ 		if (!err) {
+ 			db.users.find(function(err,docs){
+ 				res.json(docs);
+ 			});
+ 		};
+ 	});
     
 }); 
 
